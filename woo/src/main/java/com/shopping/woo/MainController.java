6e8db190df0eaa00922fd2c6b8079b61dba7aaf2
mamerongs <77999068+mamerongs@users.noreply.github.com>
2021-01-27 16:41:05 +0900

    Update MainController.java

--- woo/src/main/java/com/shopping/woo/MainController.java
+++ woo/src/main/java/com/shopping/woo/MainController.java
@@ -1,13 +1,21 @@
-package com.shopping.woo;
+package com.shopping.woo.main.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import com.shopping.woo.main.service.MainService;
 
 @Controller
 public class MainController {
     
+    @Autowired
+    private MainService mainService;
+    
     @RequestMapping("/")
     public String main() {
+        System.out.println("test!!");
+        System.out.println(mainService.getTest());
+        
         return "main";
     }
 }
