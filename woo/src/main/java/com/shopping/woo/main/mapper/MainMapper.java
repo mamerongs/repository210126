c9d99e4348f5246039bb41e6f6d453b63f3d5b6d
mamerongs <77999068+mamerongs@users.noreply.github.com>
2021-01-28 09:27:12 +0900

    Create MainMapper.java

---
+++ woo/src/main/java/com/shopping/woo/main/mapper/MainMapper.java
@@ -0,0 +1,10 @@
+package com.shopping.woo.main.mapper;
+
+import java.util.List;
+import java.util.Map;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MainMapper {
+    List<Map<String, String>> getTest();
+}
