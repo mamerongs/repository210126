bbd7312fcf60d24e9e08d047122fa4bf53712b51
mamerongs <77999068+mamerongs@users.noreply.github.com>
2021-01-28 09:25:27 +0900

    Update WooApplication.java

--- woo/src/main/java/com/shopping/woo/WooApplication.java
+++ woo/src/main/java/com/shopping/woo/WooApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WooApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WooApplication.class, args);
 	}
-
 }
+
